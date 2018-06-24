032018ed32cb48e95851065d222077a1296d9c32
Jurel P <patocjurel@gmail.com>
2018-06-24 11:29:53 +0800

    Fix path issue.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {Router,browserHistory} from 'react-router';
-import routes from 'routes';
-import 'styles/styles.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
   <Router history={browserHistory} routes={routes}/>,
